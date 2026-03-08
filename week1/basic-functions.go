28c98e5a83151759dc4a280762836f13ba471a42
victor ejike <victorejiki@gmail.com>
2026-03-08 10:17:37 +0100

    basic function in go

---
+++ week1/basic-functions.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// what i just did was to create my own function  and use it in my code in ask much
+func SayHello() {
+	fmt.Println("hello, world! am victor the software guy\n")
+}
+
+// this now re- stating it
+func main() {
+	SayHello()
+}

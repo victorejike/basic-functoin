43055fde98756f667be93361622d847bc5d56f4d
victor ejike <victorejiki@gmail.com>
2026-03-09 07:08:30 +0100

    slice implimatation

---
+++ week1/modify.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//this was calling the slice from the index of o to 6
+	price := []int{10, 20, 30, 40}
+
+	fmt.Println(price[0])
+	fmt.Println(price[2])
+}
